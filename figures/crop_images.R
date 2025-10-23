7d6985b1f152a913c0d7f7ae5033fc6418a81e1d
jriveraespejo <jriveraespejo@gmail.com>
2025-10-23 17:21:56 +0200

    section:5; process:write,code,figures

--- figures/crop_images.R
+++ figures/crop_images.R
@@ -11,6 +11,8 @@ sapply(librerias, require, character.only=T)
 dir = '/home/josema/Desktop/1. Work/1 research/PhD Antwerp/#thesis/paper3/paper3_presentation/figures'
 
 
+
+
 # Cropping images ####
 
 ## CBTL analysis ####
@@ -34,13 +36,44 @@ for( f in filenames[idx] ){
 }
 
 
+
+
+
+## ITCJ analysis ####
+
+filenames = list.files( path=file.path(dir, 'ITCJ_analysis', 'cropped'), full.names=T)
+
+idx = str_detect(filenames, 'influential')
+
+for( f in filenames[!idx] ){
+  img = image_read( path=f )
+  img = image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
+                    image=img )
+  image_write( img, path=f )
+}
+
+for( f in filenames[idx] ){
+  img = image_read( path=f )
+  img = image_crop( geometry=geometry_area(width=1770, height=1350, x_off=140, y_off=180),
+                    image=img )
+  image_write( img, path=f )
+}
+
+
+
+
 # # manual
-# img_name = '3_2_CBTL_individual_influential.png'
-# img_dir = file.path(dir, 'CBTL_analysis', img_name)
-# img = image_read( path=img_dir )
-# 
+# img = image_read( path=filenames[idx][1] )
 # print(img)
 # image_crop( geometry=geometry_area(width=1770, height=1350, x_off=140, y_off=180),
 #             image=img )
 
+# idx = str_detect(filenames, '4_6_1_ITCJ_prior')
+# filenames[idx]
+# for( f in filenames[idx] ){
+#   img = image_read( path=f )
+#   img = image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
+#                     image=img )
+#   image_write( img, path=f )
+# }
 
