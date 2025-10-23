293706702ced5fb5d85444175e0486d24be099bf
jriveraespejo <jriveraespejo@gmail.com>
2025-10-23 12:25:55 +0200

    section:5; process:write,code,figures

--- figures/crop_images.R
+++ figures/crop_images.R
@@ -4,7 +4,8 @@ rm(list=ls()); gc(); old_par=par()
 ## packages ####
 # linux: sudo apt-get install -y libmagick++-dev
 # R: install.packages('magick')
-require(magick)
+librerias = c('magick','tidyverse')
+sapply(librerias, require, character.only=T)
 
 ## dir ####
 dir = '/home/josema/Desktop/1. Work/1 research/PhD Antwerp/#thesis/paper3/paper3_presentation/figures'
@@ -12,51 +13,34 @@ dir = '/home/josema/Desktop/1. Work/1 research/PhD Antwerp/#thesis/paper3/paper3
 
 # Cropping images ####
 
-## 3_2_CBTL_stimuli_trait_comparison_plot1_2 ####
-img_name = '3_2_CBTL_stimuli_trait_comparison_plot1_2.png'
-img_dir = file.path(dir, 'CBTL_analysis', img_name)
-img = image_read( path=img_dir )
+## CBTL analysis ####
 
-print(img)
-image_info(img)
-image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
-            image=img )
+filenames = list.files( path=file.path(dir, 'CBTL_analysis', 'cropped'), full.names=T)
 
-img = image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
-                  image=img )
-image_write( img, path=img_dir )
+idx = str_detect(filenames, 'influential')
 
+for( f in filenames[!idx] ){
+  img = image_read( path=f )
+  img = image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
+                    image=img )
+  image_write( img, path=f )
+}
 
-
-
-## 3_1_CBTL_judge_trait_prior1 ####
-img_name = '3_1_CBTL_judge_trait_prior1.png'
-img_dir = file.path(dir, 'CBTL_analysis', img_name)
-img = image_read( path=img_dir )
-
-print(img)
-image_info(img)
-image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
-            image=img )
-
-img = image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
-                  image=img )
-image_write( img, path=img_dir )
+for( f in filenames[idx] ){
+  img = image_read( path=f )
+  img = image_crop( geometry=geometry_area(width=1770, height=1350, x_off=140, y_off=180),
+                    image=img )
+  image_write( img, path=f )
+}
 
 
+# # manual
+# img_name = '3_2_CBTL_individual_influential.png'
+# img_dir = file.path(dir, 'CBTL_analysis', img_name)
+# img = image_read( path=img_dir )
+# 
+# print(img)
+# image_crop( geometry=geometry_area(width=1770, height=1350, x_off=140, y_off=180),
+#             image=img )
 
 
-
-## 3_1_CBTL_stimuli_trait_prior2 ####
-img_name = '3_1_CBTL_stimuli_trait_prior2.png'
-img_dir = file.path(dir, 'CBTL_analysis', img_name)
-img = image_read( path=img_dir )
-
-print(img)
-image_info(img)
-image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
-            image=img )
-
-img = image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
-                  image=img )
-image_write( img, path=img_dir )
