9541770d85e56caf7dcf97e906e5bb0506080de4
jriveraespejo <jriveraespejo@gmail.com>
2025-10-24 11:00:21 +0200

    section:5; process:write,code,figures

--- figures/crop_images.R
+++ figures/crop_images.R
@@ -43,37 +43,37 @@ for( f in filenames[idx] ){
 
 filenames = list.files( path=file.path(dir, 'ITCJ_analysis', 'cropped'), full.names=T)
 
-idx = str_detect(filenames, 'influential')
+idx1 = str_detect(filenames, 'influential')
+idx2 = str_detect(filenames, '5_ITCJ_model_comparison')
 
-for( f in filenames[!idx] ){
+for( f in filenames[!(idx1 | idx2)] ){
   img = image_read( path=f )
   img = image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
                     image=img )
   image_write( img, path=f )
 }
 
-for( f in filenames[idx] ){
+for( f in filenames[idx1] ){
   img = image_read( path=f )
   img = image_crop( geometry=geometry_area(width=1770, height=1350, x_off=140, y_off=180),
                     image=img )
   image_write( img, path=f )
 }
 
+for( f in filenames[idx2] ){
+  img = image_read( path=f )
+  img = image_crop( geometry=geometry_area(width=1900, height=1000, x_off=0, y_off=140),
+                    image=img )
+  image_write( img, path=f )
+}
 
 
 
-# # manual
-# img = image_read( path=filenames[idx][1] )
-# print(img)
-# image_crop( geometry=geometry_area(width=1770, height=1350, x_off=140, y_off=180),
-#             image=img )
+## manual ####
+idx = str_detect(filenames, '5_ITCJ_model_comparison')
+img = image_read( path=filenames[idx] )
+print(img)
+image_crop( geometry=geometry_area(width=1900, height=600, x_off=0, y_off=140),
+            image=img )
 
-# idx = str_detect(filenames, '4_6_1_ITCJ_prior')
-# filenames[idx]
-# for( f in filenames[idx] ){
-#   img = image_read( path=f )
-#   img = image_crop( geometry=geometry_area(width=1830, height=1000, x_off=70, y_off=130),
-#                     image=img )
-#   image_write( img, path=f )
-# }
 
